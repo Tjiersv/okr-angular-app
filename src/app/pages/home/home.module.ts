aa126c34e6f6b0e7c975c542792e734ad12f1fb4
tsalaza3 <tsalaza3@bancoestado.cl>
2022-05-05 22:53:04 -0400

    feat: replace header in home component

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -5,13 +5,15 @@ import { HomeRoutingModule } from './home-routing.module';
 import { OkrCardComponent } from './components/okr-card/okr-card.component';
 import { ProgressBarComponent } from './components/progress-bar/progress-bar.component';
 import { KeyresultCardComponent } from './components/keyresult-card/keyresult-card.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
     HomeComponent,
     OkrCardComponent,
     ProgressBarComponent,
-    KeyresultCardComponent
+    KeyresultCardComponent,
+    HeaderComponent
   ],
   imports: [
     CommonModule,
